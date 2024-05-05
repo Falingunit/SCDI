b5a1e873075675ce7f3a526b267596bd7d6de245
sbani <sbani@FALINGUNIT>
2024-05-05 18:15:37 +0530

    Initial commit

---
+++ src/main/java/net/falingunit/skibidicertifieddiscordintegrator/SCDIClient.java
@@ -0,0 +1,10 @@
+package net.falingunit.skibidicertifieddiscordintegrator;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SCDIClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/falingunit/skibidicertifieddiscordintegrator/SCDIDataGenerator.java
@@ -0,0 +1,11 @@
+package net.falingunit.skibidicertifieddiscordintegrator;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SCDIDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
